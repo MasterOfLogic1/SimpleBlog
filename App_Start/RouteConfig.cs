f73a576b8b9633eae0d62b0fe48d4c45560210c7
David Oku <David.Oku@skipton.co.uk>
2025-06-27 10:38:10 +0100

    Add project files.

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,34 @@
+﻿using SimpleBlog.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SimpleBlog
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            var namespaces = new [] {typeof(PostsController).Namespace};
+
+            routes.MapRoute(
+                name: "Home",
+                url: "",
+                defaults: new { controller = "Posts", action = "Index" },
+                namespaces
+            );
+
+            routes.MapRoute(
+                name: "Login",
+                url: "login",
+                defaults: new { controller = "Auth", action = "Login" },
+                namespaces
+             );
+        }
+    }
+}
---
+++ Controllers/PostsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Controllers
+{
+    public class PostsController:Controller
+    {
+        public ActionResult Index()
+        {
+            // This is where you would normally retrieve posts from a database
+            // For now, we will return a simple view
+            return Content("Hello World");
+        }
+    }
+}
